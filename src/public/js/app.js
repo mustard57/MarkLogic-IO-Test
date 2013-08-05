c2dd80d556e69e1eea7fa04a7206823e45b09891
Ken Tune <ken.tune@marklogic.com>
2013-08-05 23:44:43 +0100

    20130805b

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -20,3 +20,25 @@ function validate()
         }
     }); 
 }
+function validate_study()
+{
+    $.ajax({
+        url:"/app/ui/ajax/field-validation-for-study.xqy?name="+this.id+"&value="+this.value,
+        id : this.id,
+        method:"get",
+        dataType:"json",
+        success : function(response,status,xhr) {
+        	var key = this.id.replace("-values","").replace("-default","");
+        	if(response[key])
+    		{
+        		$("#"+this.id+"_comment").text(response[key]);
+        		$("#"+this.id).focus().addClass("errorcell");        		
+    		}
+        	else
+    		{
+        		$("#"+this.id+"_comment").text("");
+        		$("#"+this.id).removeClass("errorcell");        		
+    		}
+        }
+    }); 
+}
